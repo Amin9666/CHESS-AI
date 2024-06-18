deeca26d6e972e1f8452c5eef43318e77ce6ae3b
AminWeinman <aminweinman@gmail.com>
2024-06-18 14:12:52 -0600

    added en-passant feature, castling, and promotion. Feature for R and T key

--- src/main.py
+++ src/main.py
@@ -2,19 +2,17 @@ import pygame
 import sys
 
 from const import *
-from game  import Game
+from game import Game
 from square import Square
 from move import Move
 
 class Main:
-    
+
     def __init__(self):
         pygame.init()
-        #self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
         pygame.display.set_caption('Chess')
         self.game = Game()
-        
 
     def mainloop(self):
         
@@ -22,50 +20,51 @@ class Main:
         game = self.game
         board = self.game.board
         dragger = self.game.dragger
-        
-        
+
         while True:
+            # show methods
             game.show_bg(screen)
             game.show_last_move(screen)
             game.show_moves(screen)
             game.show_pieces(screen)
             game.show_hover(screen)
-            
+
             if dragger.dragging:
                 dragger.update_blit(screen)
-            
+
             for event in pygame.event.get():
-                
-                #click
+
+                # click
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     dragger.update_mouse(event.pos)
 
-                    clicked_row = dragger.mouseY // SQSIZE 
-                    clicked_col = dragger.mouseX // SQSIZE 
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
 
-                    #if clicked square has a piece
+                    # if clicked square has a piece ?
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
-                        # valid piece color ?
-                        if piece.color == game.next_player:   
-                            board.calc_moves(piece, clicked_row, clicked_col)
+                        # valid piece (color) ?
+                        if piece.color == game.next_player:
+                            board.calc_moves(piece, clicked_row, clicked_col, bool=True)
                             dragger.save_initial(event.pos)
                             dragger.drag_piece(piece)
-
+                            # show methods 
                             game.show_bg(screen)
                             game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_pieces(screen)
-
-                    
-                #mouse motion
+                
+                # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
                     motion_row = event.pos[1] // SQSIZE
                     motion_col = event.pos[0] // SQSIZE
+
                     game.set_hover(motion_row, motion_col)
-                    #show methods
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
+                        # show methods
                         game.show_bg(screen)
                         game.show_last_move(screen)
                         game.show_moves(screen)
@@ -73,52 +72,67 @@ class Main:
                         game.show_hover(screen)
                         dragger.update_blit(screen)
                 
-                #clik release
+                # click release
                 elif event.type == pygame.MOUSEBUTTONUP:
+                    
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
-                        
+
                         released_row = dragger.mouseY // SQSIZE
                         released_col = dragger.mouseX // SQSIZE
-                        
+
+                        # create possible move
                         initial = Square(dragger.initial_row, dragger.initial_col)
                         final = Square(released_row, released_col)
                         move = Move(initial, final)
-                        
-                        #valid move?
+
+                        # valid move ?
                         if board.valid_move(dragger.piece, move):
+                            # normal capture
                             captured = board.squares[released_row][released_col].has_piece()
                             board.move(dragger.piece, move)
-                            #sound
+
+                            board.set_true_en_passant(dragger.piece)                            
+
+                            # sounds
                             game.play_sound(captured)
-                            #methods
+                            # show methods
                             game.show_bg(screen)
                             game.show_last_move(screen)
                             game.show_pieces(screen)
-                            
-                            #next turn
+                            # next turn
                             game.next_turn()
-                            
-                            
-                    dragger.undrag_piece()
                     
-                #key press
+                    dragger.undrag_piece()
+                
+                # key press
                 elif event.type == pygame.KEYDOWN:
                     
-                    #Changing themes
-                    if event.key == pygame.K_t: 
+                    # changing themes
+                    if event.key == pygame.K_t:
                         game.change_theme()
-                
-                #quit 
+
+                    # resetting game
+                    if event.key == pygame.K_r:
+                        game.reset()
+                        game = self.game
+                        board = self.game.board
+                        dragger = self.game.dragger
+
+                    # toggle fullscreen/windowed mode
+                    if event.key == pygame.K_f:
+                        if self.screen.get_flags() & pygame.FULLSCREEN:
+                            pygame.display.set_mode((WIDTH, HEIGHT))
+                        else:
+                            pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+
+                # quit application
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             
-            
-            
-                       
-            pygame.display.update()    
-    
+            pygame.display.update()
+
+
 main = Main()
 main.mainloop()
-
